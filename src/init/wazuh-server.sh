10f3014be5348bff3e3083558ac6b0c9a7b5c28c
pereyra-m <matias.pereyra@wazuh.com>
2021-01-06 17:55:00 -0300

    Updating INITCONF variable in wazuh-server.sh

--- src/init/wazuh-server.sh
+++ src/init/wazuh-server.sh
@@ -22,7 +22,7 @@ fi
 
 AUTHOR="Wazuh Inc."
 USE_JSON=false
-INITCONF="/etc/ossec-init.conf"
+INITCONF=$DIR"/etc/ossec-init.conf"
 DAEMONS="wazuh-clusterd wazuh-modulesd wazuh-monitord wazuh-logcollector wazuh-remoted wazuh-syscheckd wazuh-analysisd wazuh-maild wazuh-execd wazuh-db wazuh-authd wazuh-agentlessd wazuh-integratord wazuh-dbd wazuh-csyslogd wazuh-apid"
 OP_DAEMONS="wazuh-clusterd wazuh-maild wazuh-agentlessd wazuh-integratord wazuh-dbd wazuh-csyslogd"
 
